3ea097213a115cacb6a776f4ab38e489706c4258
Tomasz Braczyński <tombra@st.amu.edu.pl>
2020-03-07 17:00:31 +0100

    - 50% of cases are working fine :D

--- PrimeChecker/PrimeChecker/Program.cs
+++ PrimeChecker/PrimeChecker/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace PrimeChecker
 {
     class Program
     {
+        // 14484968830081
+        // 8142915490464301747219
+        // 6214962790459424018719016702779392029
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj liczbę do sprawdzenia");
+            string numberAsString = Console.ReadLine();
+            BigInteger numberToCheck = BigInteger.Parse(numberAsString);
+            if (CheckIfNumberIsPrime(numberToCheck))
+            {
+                Console.WriteLine("Liczba jest liczbą pierwszą.");
+            }
+            else
+            {
+                Console.WriteLine("Liczba jest liczbą złożoną.");
+            }
+            Console.ReadKey();
+        }
+
+        static bool CheckIfNumberIsPrime(BigInteger numberToCheck)
+        {
+            if (numberToCheck.IsEven && numberToCheck > 2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
